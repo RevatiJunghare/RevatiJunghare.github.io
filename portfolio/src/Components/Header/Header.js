f94753d11cf1fed541a10eea2536719ba1f6c1d7
revati junghare <revati.junghare@geta.ai>
2024-02-27 23:05:12 +0530

    new link added

--- portfolio/src/Components/Header/Header.js
+++ portfolio/src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import {Link} from "react-scroll"
 import Button from "react-bootstrap/Button";
-import resume from "../../Assets/fp05_371-Revati_Junghare_Resume.pdf"
+import resume from "../../Assets/Revati-Junghare-Resume_fp05_371.pdf"
 
 
 function Header() {
@@ -120,7 +120,7 @@ function Header() {
                 className="fork-btn-inner"
                 onClick={() => {
                     window.open(
-                      "https://drive.google.com/file/d/1TtgcZ8Zyzhe2HEFGwnL-RB-QgCnPbas_/view?usp=share_link"
+                      "https://drive.google.com/file/d/1g9VFympdgGNaaMLcM6Yt3MKbQBqPCYyD/view?usp=sharing"
                     );
                   }}
               >
